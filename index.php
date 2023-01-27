fb06f32c255c2a98d38863df8f2679f1780c8a55
Noémie <noemie@ndasilva.ca>
2023-01-27 11:09:00 -0500

    Ajout d'une balise div au fichier index.php et modification de style (style.css)

--- index.php
+++ index.php
@@ -6,16 +6,18 @@
 ?>
 <?php get_header(); ?>
 <main>
+  <div>
 <?php 
 if(have_posts()): 
   while (have_posts()) : the_post();
-the_title('<h1>', '</h1>');
+the_title('<h2>', '</h2>');
 the_content(); ?>
 <hr>
 <?php
 endwhile;
 endif;
 ?>
+</div>
 
 </main>
 <?php get_footer(); ?>
