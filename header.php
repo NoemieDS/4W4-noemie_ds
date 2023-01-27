0e745b2975f27a67c9353266d11328465385ee61
Noémie <noemie@ndasilva.ca>
2023-01-27 10:48:41 -0500

    Ajout d'une boucle while

--- header.php
+++ header.php
@@ -4,9 +4,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
- 
-</head>
-<?php wp_head(); ?>
+  <?php wp_head(); ?>
+ </head>
 <body>
   <header>
     <h1>4W4 - Création d'interface et développement Web</h1>
--- index.php
+++ index.php
@@ -5,5 +5,17 @@
  */
 ?>
 <?php get_header(); ?>
-<h1>Bienvenue au cours de 4W4</h1>
+<main>
+<?php 
+if(have_posts()): 
+  while (have_posts()) : the_post();
+the_title('<h1>', '</h1>');
+the_content(); ?>
+<hr>
+<?php
+endwhile;
+endif;
+?>
+
+</main>
 <?php get_footer(); ?>
