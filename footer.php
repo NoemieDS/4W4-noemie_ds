4bcd84b6bc2d0742bdfbb0f95cc8a2d53e2f95b1
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-02-03 09:30:45 -0500

    Modification de style : liens et footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
-
-<footer>
-<h3>Le pied de page</h3>
+<footer class="site__footer">
+  <section >Colonne 1</section>
+  <section>Colonne 2</section>
+  <section>Colonne 3</section>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -8,9 +8,6 @@
  </head>
 <body>
   <header>
-  <!-- <h3>- <?= bloginfo('name')?></h3>
-    <h3>- <?= bloginfo('description')?></h3>
-   <h3>- <?= bloginfo('url')?></h3> -->
     <h1><a href="<?= bloginfo('url')?>"><?= bloginfo('name')?></a></h1>
     <h2><?= bloginfo('description')?></h2>
 </header>
