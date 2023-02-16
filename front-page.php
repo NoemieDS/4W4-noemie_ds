82180d4fb0f18b058a7f511da46124d3c97c40d7
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-02-15 19:37:45 -0500

    S4S1 variables couleurs et font-family, integration de nouvelles polices (Google Font)

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@
     <?php
     if (have_posts()) :
       while (have_posts()) : the_post(); ?>
-        <article>
+        <article class="front_article">
           <h2>
             <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?> </a>
           </h2>
--- header.php
+++ header.php
@@ -4,6 +4,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;1,400&family=Source+Sans+3:ital,wght@0,400;1,400&family=Source+Serif+Pro:ital,wght@0,600;1,600&display=swap" rel="stylesheet"> 
   <?php wp_head(); ?>
  </head>
 <body>
