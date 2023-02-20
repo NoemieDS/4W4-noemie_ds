cdab1d225bbe551b4e93be96eab2f77432b40b2b
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-02-20 10:17:46 -0500

    S4C2 Changements stylistiques, ajout normalize box-sizing

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
 ?>
 <?php get_header(); ?>
 <main>
-  <h3>Les derniers articles de front-page.php</h3>
+  <h2 class="front_titre">Les derniers articles de front-page.php</h2>
   <section class="blocflex">
     <?php
     if (have_posts()) :
@@ -22,8 +22,7 @@
           <?php //the_excerpt(); //Affiche un résumé de l'article 
           ?>
           <p><?php if(is_category('cours'))  echo 'cours';?></p>
-          <p> <?= wp_trim_words(get_the_excerpt(), 20, "<span> &#10148;</span>") ?> </p>
-          
+          <p> <?= wp_trim_words(get_the_excerpt(), 20, "<span> &#187;</span>") ?> </p>
         </article>
     <?php
       endwhile;
