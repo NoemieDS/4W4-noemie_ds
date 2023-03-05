12f2f275244ef094dc368796528086a65e287839
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-03-05 11:56:54 -0500

    S6C2 Modif couleur liens asside, tentative d'afficher le titre des articles de galerie dans la catégorie galerie (mais pas sur l'accueil) - non fonctionnel

--- template-parts/categorie-galerie.php
+++ template-parts/categorie-galerie.php
@@ -1,11 +1,29 @@
 <?php
 /* 
 * Template part pour afficher la galerie dans la page d'accueil
+*
+* COMMENT NE PAS AFFICHER LE TITRE SUR LA PAGE D'ACCUEIL ??
 */
+$titre = get_the_title();
+$lien = get_permalink(); 
+$accueil = get_home_url();
 ?>
 
+
+
 <section class="blocflex__galerie"> 
-<?php
+
+<?php 
+
+if(!$accueil)
+ {?>
+   <h3><a href="<?php the_permalink(); ?>"> <?= $titre ?></a></h3>
+   <?php
+}
+?>
+
+   <!--  <?php $lire = "<span><a href='" . $lien . "'>... &#187;</a></span>" ?> -->
+  <?php
    the_content();
 ?>
 </section>
