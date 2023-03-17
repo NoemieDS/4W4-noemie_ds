8fe269b41ceb890f92a2f979217cf1dcc3e6d3e0
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-03-17 11:29:02 -0400

    S8C2 Version finale du TP 1

--- footer.php
+++ footer.php
@@ -1,10 +1,19 @@
 <!-- fermeture div calss .site du header -->
 </div>
 <footer class="site__footer">
-<section >Github</section>
-  <section>Noémie da Silva</section>
-  <section>2023</section>
+  <section> <?php the_custom_logo() ?>    
+    </section>
+  <section>
+    <?php wp_nav_menu(array(
+      "menu" => "pied2page",
+      "container" => "nav"
+    )) ?>
+  </section>
+  <section class="footer-date">
+  <?= date("Y") ?>
+  </section>
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
