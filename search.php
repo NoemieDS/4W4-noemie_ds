74bbd6578e0d283270815ff4aef3aab57a8654fc
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-03-31 10:00:59 -0400

    S9C2 Examen intra flexbox adaptatif tel la maquette

--- search.php
+++ search.php
@@ -7,22 +7,25 @@
 ?>
 <?php get_header(); ?>
 <main class="site__main no-aside">
-<!--   <h5 class="modele_php">search.php</h5>  -->
+  <!--   <h5 class="modele_php">search.php</h5>  -->
   <h2 class="search_titre">Résultats de la recherche</h2>
-  <article class="search_resultat>
-          <?php
-      if (have_posts()) :
-        while (have_posts()) : the_post();
-       $lien = get_permalink(); 
-        $lire = "<a href='" . $lien . "'> [...]</a>"; ?>
+
+  <?php
+  if (have_posts()) :
+    while (have_posts()) : the_post();
+      $lien = get_permalink();
+      $lire = "<a href='" . $lien . "'> [...]</a>"; ?>
+      <article class="search_resultat">
         <div class="search_resultat_titre">
-        <h4 ><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h4></div>
-        <div class="search_resultat_extrait">   <?= wp_trim_words(get_the_excerpt(), 50, $lire) ?></div>
-          <hr>
-          <?php
-        endwhile;
-      endif;
-      ?>
-   </article>
+          <h3><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h3>
+        </div>
+        <div class="search_resultat_extrait"> <?= wp_trim_words(get_the_excerpt(), 50, $lire) ?></div>
+      </article>
+      <hr class="search_diviseur">
+  <?php
+    endwhile;
+  endif;
+  ?>
+
 </main>
 <?php get_footer(); ?>
