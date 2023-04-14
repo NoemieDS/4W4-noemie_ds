9377758a0272f33f389a7101bca1ec1f6e7544a9
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-04-14 11:24:35 -0400

    S11C1 lab4 Ajout ACF pour la galerie

--- template-parts/categorie-galerie.php
+++ template-parts/categorie-galerie.php
@@ -14,6 +14,9 @@ if(!is_front_page()) { ?>
 <h3><a href="<?php the_permalink(); ?>"> <?= $titre ?></a></h3> 
 <?php }
  the_content();
-?>
+ ?>
+ <p><?php the_field('description'); ?>
+  <br/>Par <?php the_field('auteur'); ?>,
+   <?php the_field('date'); ?></br>
 
 </section>
