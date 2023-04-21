b2d04c7b95e401737baf432cbc73b46da3ce287d
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-04-21 09:57:56 -0400

    S12C1 Ajout d'un 3e sidebar / widget, le footer a 2 lignes, la première ligne contient 3 sections (logo et 2 widgets), la 2e ligne contient 1 widget.

--- footer.php
+++ footer.php
@@ -1,22 +1,21 @@
 <!-- fermeture div calss .site du header -->
 </div>
 <footer class="site__footer">
-  <section> <?php the_custom_logo() ?>    
+  <div class="footer_ligne1">
+    <section>
+      <?php the_custom_logo() ?>
     </section>
-  <section>
-    <div class="sidebar">
-    <?php dynamic_sidebar( 'footer_1' ); ?>
-</div>
-<div class="footer-date">
-<?= date("Y") ?>
-</div>
-  </section>
-  <section >
- 
-  <div class="sidebar">
-    <?php dynamic_sidebar( 'footer_2' ); ?>
-</div>
-  </section>
+    <section class="sidebar">
+      <?php dynamic_sidebar('footer_1'); ?>
+    </section>
+    <section class="sidebar">
+      <?php dynamic_sidebar('footer_2'); ?>
+    </section>
+  </div>
+  <div class="footer-ligne2">
+  <?php dynamic_sidebar('footer_3'); ?>
+   <p> <?= date("Y") ?> </p>
+  </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- functions.php
+++ functions.php
@@ -120,5 +120,15 @@ function enregistrer_sidebar() {
         'before_title' => '<h2 class="widget-title">',
         'after_title' => '</h2>',
     ) );
+
+    register_sidebar( array(
+        'name' => __( 'Footer 3', '4w4-noemie_ds' ),
+        'id' => 'footer_3',
+        'description' => __( 'Une zone pour afficher des widgets dans le footer.', '4w4-noemie_ds' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ) );
 }
 add_action( 'widgets_init', 'enregistrer_sidebar' );
